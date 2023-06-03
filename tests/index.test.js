1dbffa70bb243cdafcd41362c5f50cc801893e8d
kiarash98e <kiarashsafaei0@gmail.com>
2023-06-03 20:53:02 +0430

    modify testing file

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,1 +1,52 @@
-const {} = require("./index.js")
+const { hello, even, isGreat, shoppingList, sum } = require("./index.js")
+
+test('two plus two is four', () => {
+    expect(2 + 2).toBe(4);
+});
+
+test('there is no I in team', () => {
+    expect(hello).not.toMatch(/I/);
+    expect(hello).toMatch(/hello world !!!/);
+});
+
+test('two plus two', () => {
+    const value = 2 + 2;
+    expect(value).toBeGreaterThan(3);
+    expect(value).toBeGreaterThanOrEqual(3.5);
+    expect(value).toBeLessThan(5);
+    expect(value).toBeLessThanOrEqual(4.5);
+  
+    // toBe and toEqual are equivalent for numbers
+    expect(even).toBe(12);
+    expect(even).toEqual(4);
+});
+
+
+test("is greate boolean value",() => {
+    expect(isGreat).toBeTruthy()
+})
+
+test('the shopping list has milk on it', () => {
+    expect(shoppingList).toContain('milk');
+    expect(new Set(shoppingList)).toContain(
+      [  'kleenex',
+        'trash bags',
+        'paper towels',
+        'milk',
+      ]);
+  });
+
+  test('object assignment', () => {
+    const data = {one: 1};
+    data['two'] = 2;
+    expect(data).toEqual({one: 1, two: 2});
+});
+
+test('func test', () => {
+    
+    expect(sum(2,2)).toEqual(4);
+    expect(sum(2,"hello")).toThrow(TypeError);
+  });
+
+
+
